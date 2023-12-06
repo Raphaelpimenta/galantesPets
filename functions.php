1387a44c19d9593171c3752c4f5ab6dd5194fae3
Raphael Pimenta <raphael.pimentagomes@hotmail.com>
2023-12-06 05:18:23 -0300

    function de tamanho de img

--- functions.php
+++ functions.php
@@ -4,4 +4,11 @@
 require_once get_template_directory() . '/cmb2/load.php';
 
 
+// Custom Images Sizes
+function my_custom_sizes() {
+	add_image_size('large', 1400, 380, true);
+	add_image_size('medium', 768, 380, true);
+}
+add_action('after_setup_theme', 'my_custom_sizes');
+
 ?>
