1defbfbfeb51a31810f19dedcd7e22d32785cf2e
Raphael Pimenta <raphael.pimentagomes@hotmail.com>
2023-12-21 18:27:43 -0300

    atualização de pasta

---
+++ inc/planos.php
@@ -0,0 +1,32 @@
+<article class="container">
+        <div class="planos">
+            <h2>Planos<span>.</span></h2>
+
+            <div class="planos-item">
+                <h3>Quinzenal</h3>
+                <span>R$ 99,00</span>
+                <ul class="plano-lista">
+                    <li>Banho e Tosa</li>
+                    <li>Creche</li>
+                    <li>Dog Walker</li>
+                    <li>Táxi Dog - 08h às 18h</li>
+                </ul>
+                <button class="btn-padrao">Inscreva-se</button>
+            </div>
+
+            <div class="planos-item">
+                <h3>Mensal</h3>
+                <span>R$ 199,00</span>
+                <ul class="plano-lista">
+                    <li>Banho e Tosa + creche</li>
+                    <li>Táxi Dog 24h</li>
+                    <li>Adestramento</li>
+                    <li>Desconto em hotel</li>
+                    <li>Desconto em clínicas ceterinárias</li>
+                    <li>Duas consultas veterinária por mês</li>
+                </ul>
+                <button class="btn-padrao">Inscreva-se</button>
+            </div>
+
+        </div>
+    </article>
